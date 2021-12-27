ef04465ce8898a0553e13ac3858ce73d4408c66f
max <msuvorov7@gmail.com>
2021-12-27 23:46:42 +0300

    [feature-1] Начало реализации группировщика файлов

--- internal/pkg/grouper/grouper.py
+++ internal/pkg/grouper/grouper.py
@@ -1,3 +1,5 @@
+import os
+
 from configs.config_grouper import config_name
 from internal.pkg.base.base import Base
 
@@ -7,8 +9,15 @@ class Grouper(Base):
         super().__init__()
         self.new_name = config_name
 
+    def get_visible_files(self, path='.'):
+        all_files = os.listdir(path)
+        files = list(filter(lambda x: os.path.isfile(path + '/' + x), all_files))
+        hidden_files = list(filter(lambda x: x.startswith('.'), files))
+        return list(set(files) - set(hidden_files))
+
 
 if __name__ == "__main__":
     grouper = Grouper()
     print(grouper.new_name)
     print(grouper.name)
+    print(grouper.get_visible_files())
